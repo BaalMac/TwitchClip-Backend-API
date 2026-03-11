398bf5b655b06767a2d63c8960f37530a18cf65f
BaalMac <macshadow3030@gmail.com>
2026-03-11 02:56:09 -0700

    Setup Project folder structure and plans on each of the scripts

---
+++ app/app.py
@@ -0,0 +1,16 @@
+'''
+Before separating the individual define function
+Start creating the Entire code here
+
+Plans: 
+- Setup Database structure
+- Call Database events such as [Event_History] and [Event_Time]
+and return them as a JSON format
+- Communicate to Discord bot
+
+ONCE THE MAIN FUNCTION IS COMPLETE - ENGAGE ZERO TRUST INPUT - CLEAN/VALIDATE ALL INPUTS
+Note to self: 
+> Impliment rate limiting in API
+'''
+
+from config import Config
---
+++ config.py
@@ -0,0 +1,20 @@
+'''
+Config.py holding data for .env so that the rest of the API can grab the vital information
+For now since this is still in development. the Config.py will be holding all the information
+Once During Production, Separate the data to where the machines with the Application, only has
+its necessary credentials to function. (dont forget to recreate .env once in production)
+'''
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+class Config:
+    API_PORT     = os.getenv('API_PORT', 5000)  # ← 5000 is the default port for Flask
+    SECRET_KEY   = os.getenv('SECRET_KEY')
+    DATABASE_URL = os.getenv('DATABASE_URL')
+
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+    TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+    TWITCH_CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
+ 
